879eb873bdc88054e1bea37756af1dec1f441a60
emre-unaldi <emree.unaldi@gmail.com>
2021-12-27 18:12:18 +0300

    Users Modelinin Oluşturulması

---
+++ models/Users.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    googleId: {
+        type: String,
+        unique: true
+    },
+    name: String,
+    surname: String,
+    profilePhotoUrl: String
+});
+
+module.exports = mongoose.model('users', userSchema);
+// users -> collection adı , userSchema -> model adı 
