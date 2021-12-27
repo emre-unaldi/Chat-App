0469f888d68923b07f741d74fe6bf09a479cdeae
emre-unaldi <emree.unaldi@gmail.com>
2021-12-27 23:52:50 +0300

    İndex Router

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) =>{
-  res.render('index', { title: 'ChatApp' });
+  if (!req.user) {  // user yoksa index sayfasını render etsin
+    res.render('index', { title: 'ChatApp' });
+  } else { // user varsa chat router'ına yönlendirsin.
+    res.redirect('/chat'); 
+  }
+  
 });
 
 module.exports = router;
