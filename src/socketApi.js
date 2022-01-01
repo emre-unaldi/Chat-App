4dd174e973fd0ce64a3d7aeb53e0bb4273417086
emre-unaldi <emree.unaldi@gmail.com>
2022-01-01 19:22:43 +0300

    Socket Authorization

--- src/socketApi.js
+++ src/socketApi.js
@@ -1,19 +1,28 @@
 const socketio = require('socket.io');
+const socketAuthorization = require('../middleware/socketAuthorization');
 const io = socketio();
 
 const socketApi = {
     io // io: io demektir. es6
 };
 
+/*
+  Socket Authorization
+    socketio da bir middleware yazmak için io.use() dememiz yeterli.
+    - Her socket bağlantısı çalıtırılma / kullanılmak istendiğinde arada bu middleware 
+    olacak ve her ilem bu ara katmandan geçtikten sonra sonuca ulaşacak.
+*/
+io.use(socketAuthorization);
+
 // Redis Adapter
-const redisAdapter = require('socket.io-redis');
-io.adapter(redisAdapter({
+const redisAdapter = require("socket.io-redis");
+io.adapter(redisAdapter({ 
     host: process.env.REDIS_URI, 
     port: process.env.REDIS_PORT
 }));
 
 io.on('connection', (socket) => {
-    console.log('a user logged in');
+    console.log('a user logged in with name ' + socket.request.user.name);
 
     socket.broadcast.emit('hello');
 });
