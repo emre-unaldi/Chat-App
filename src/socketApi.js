493451087e11dd2e5f0397f1414ab248d9c7c66b
emre-unaldi <emree.unaldi@gmail.com>
2022-01-01 16:17:40 +0300

    SocketIO Redis Adapter

--- src/socketApi.js
+++ src/socketApi.js
@@ -5,8 +5,17 @@ const socketApi = {
     io // io: io demektir. es6
 };
 
+// Redis Adapter
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({
+    host: process.env.REDIS_URI, 
+    port: process.env.REDIS_PORT
+}));
+
 io.on('connection', (socket) => {
     console.log('a user logged in');
+
+    socket.broadcast.emit('hello');
 });
 
 module.exports = socketApi;
